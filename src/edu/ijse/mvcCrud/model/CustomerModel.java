36893cab4581199e603a79b333b50f25f02b4344
kvn2004 <knimsaravihanga@gmail.com>
2024-09-11 12:54:58 +0530

    update Customer model

--- src/edu/ijse/mvcCrud/model/CustomerModel.java
+++ src/edu/ijse/mvcCrud/model/CustomerModel.java
@@ -17,21 +17,39 @@ public class CustomerModel {
     public int saveCustomer(CustomerDTO customerDTO) throws ClassNotFoundException, SQLException {
         Connection connection = DBConnection.getInstance().getConnection();
         String sql = "INSERT INTO Customer VALUES(?,?,?,?,?,?,?,?,?)";
-        PreparedStatement stm= connection.prepareStatement(sql);
+        PreparedStatement stm = connection.prepareStatement(sql);
 
-        stm.set(customerDTO.getId(), 1);
-        connection.prepareStatement(customerDTO.getTitle(), 2);
-        connection.prepareStatement(customerDTO.getName(), 3);
-        connection.prepareStatement(customerDTO.getDob(), 4);
-        connection.prepareStatement(customerDTO.getSalary() + "", 5);
-        connection.prepareStatement(customerDTO.getAddress(), 6);
-        connection.prepareStatement(customerDTO.getCity(), 7);
-        connection.prepareStatement(customerDTO.getProvince(), 8);
-        connection.prepareStatement(customerDTO.getPostalCode(), 9);
-        
-        stm.executeUpdate();
+        stm.setString(1, customerDTO.getId());
+        stm.setString(2, customerDTO.getTitle());
+        stm.setString(3, customerDTO.getName());
+        stm.setString(4, customerDTO.getDob());
+        stm.setString(5, customerDTO.getSalary() + "");
+        stm.setString(6, customerDTO.getAddress());
+        stm.setString(7, customerDTO.getCity());
+        stm.setString(8, customerDTO.getProvince());
+        stm.setString(9, customerDTO.getPostalCode());
 
-        return 0;
+        return stm.executeUpdate();
+
+    }
+    
+     public int updateCustomer(CustomerDTO customerDTO) throws ClassNotFoundException, SQLException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        String sql ="UPDATE Customer SET CustTitle = ?, CustName = ?, DOB = ?, salary = ?, CustAddress = ?, City = ?, Province = ?, PostalCode = ? WHERE CustID = ?";
+
+        PreparedStatement stm = connection.prepareStatement(sql);
+
+        stm.setString(9, customerDTO.getId());
+        stm.setString(1, customerDTO.getTitle());
+        stm.setString(2, customerDTO.getName());
+        stm.setString(3, customerDTO.getDob());
+        stm.setString(4, customerDTO.getSalary() + "");
+        stm.setString(5, customerDTO.getAddress());
+        stm.setString(6, customerDTO.getCity());
+        stm.setString(7, customerDTO.getProvince());
+        stm.setString(8, customerDTO.getPostalCode());
+
+        return stm.executeUpdate();
 
     }
 
